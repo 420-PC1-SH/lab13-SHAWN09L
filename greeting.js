6a6a5b303c2fc1ac00af663c8c72aee185889ab7
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-07-15 09:49:24 -0400

    exercice 2 - requis 1 - pass

---
+++ greeting.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    let text = "Hello, " + String(name);
+    return text;
+}
+
+module.exports = greet;
--- greeting.test.js
+++ greeting.test.js
@@ -4,6 +4,7 @@ describe("Greeting", function () {
 
     test("takes a string 'name' and returns 'Hello, {name}", function () {
         let result = greeting("Luke");
-        expect(typeof result).toEqual("String");
+        expect(typeof result).toEqual("string");
     })
 })
+
