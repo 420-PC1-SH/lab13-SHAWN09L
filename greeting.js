e13aff55ca71759f5f4d12e2b76d002136ec15f5
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-07-15 10:12:32 -0400

    exercice 2 - requis 4 - pass

--- greeting.js
+++ greeting.js
@@ -1,6 +1,10 @@
 function greet(name) {
+    console.log(typeof name);
     if (name == null || name == undefined || name == "") {
         return "Hello there"
+    }
+    if (name.length == 2) {
+         return "Hello, " + String(name[0]) + " and " + String(name[1]);
     }
     if (name == name.toUpperCase()) {
         return "HELLO, " + String(name);
